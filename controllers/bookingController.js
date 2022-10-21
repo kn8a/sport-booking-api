0a9c5d620317cb9086b2857d5bab61896a8ef243
kn8a <88045655+kn8a@users.noreply.github.com>
2022-10-21 18:45:54 +0700

    api builds and returns times for given date

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,7 +3,34 @@ const asyncHandler = require("express-async-handler")
 
 const checkAvailability = asyncHandler(async(req,res) => {
     console.log(req.params)
-    return
+    //seperate date
+    const date= {
+        year: Number(req.params.date.slice(0,4)),
+        month: Number(req.params.date.slice(5,7)),
+        day: Number(req.params.date.slice(8))
+    }
+    //get booking for date - add Select only slots
+    const existingBookings = await Booking.find({year:date.year, month:date.month, day:date.day})
+    console.log(existingBookings)
+
+    const buildTimes = () => {
+        let slots= []
+        for (let i=7; i<22; i++) {
+            
+            let price
+            if (i<18) {
+                price = 50
+            } else {
+                price = 100
+            }
+          slots.push({time: `${i}:00`, value: i, price: price})
+          slots.push({time: `${i}:30`, value: i+0.5, price: price})
+        }
+        return slots
+      }
+      const times = buildTimes()
+      console.log(times)
+    res.status(200).json({times: times})
 })
 
 module.exports = {
