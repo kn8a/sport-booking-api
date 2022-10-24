bee5b7e91909cf6aea29950b461ccfa1ee7d432e
kn8a <88045655+kn8a@users.noreply.github.com>
2022-10-25 00:08:09 +0700

    new booking availability validate & total amount

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,31 +1,64 @@
 const Booking = require("../models/bookingModel")
 const asyncHandler = require("express-async-handler")
 
-const checkAvailability = asyncHandler(async(req,res) => {
-    console.log(req.params)
+function buildTimes(bookedSlots) {
+    let slots= []
+    
+    for (let i=7; i<22; i++) {
+
+        let booked = false
+        if (bookedSlots.indexOf(i) != -1) {
+            booked = true
+        }
+        
+        let price
+        if (i<18) {
+            price = 50
+        } else {
+            price = 100
+        }
+      slots.push({time: `${i}:00`, value: i, price: price, booked: booked})
+      slots.push({time: `${i}:30`, value: i+0.5, price: price, booked: booked})
+    }
+    return slots
+  }
+
+  function dateSlicer(date) {
+    return({
+        year: Number(date.slice(0,4)),
+        month: Number(date.slice(5,7)),
+        day: Number(date.slice(8))
+    })
+  }
+
+  function futureDateChecker(reqDate) {
     let currentDate = new Date();
     let cDay = currentDate.getDate();
     let cMonth = currentDate.getMonth() + 1;
     let cYear = currentDate.getFullYear();
-    //seperate date
-
-    const date= {
-        year: Number(req.params.date.slice(0,4)),
-        month: Number(req.params.date.slice(5,7)),
-        day: Number(req.params.date.slice(8))
-    }
-
     if (
-        (cYear == date.year && cMonth == date.month && cDay > date.day) ||
-        (cYear == date.year && cMonth > date.month) ||
-        (cYear > date.year)
+        (cYear == reqDate.year && cMonth == reqDate.month && cDay > reqDate.day) ||
+        (cYear == reqDate.year && cMonth > reqDate.month) ||
+        (cYear > reqDate.year)
     ) {
+        return true
+    }
+  }
+
+
+
+//*check availability
+const checkAvailability = asyncHandler(async(req,res) => {
+    
+    const date= dateSlicer(req.params.date)
+    if (futureDateChecker(date)) {
         res.status(400).json({message: "You cannot book in the past. Please select a future date"})
         return
     }
     
-    //get booking for date - add Select only slots
-    const existingBookings = await Booking.find({year:date.year, month:date.month, day:date.day}).select({slots:1}).populate('slots')
+    
+    //*get booking for date - add Select only slots
+    const existingBookings = await Booking.find({year:date.year, month:date.month, day:date.day}).select({slots:1})
     console.log(existingBookings)
     let bookedSlots=[]
     if (existingBookings) {
@@ -35,40 +68,56 @@ const checkAvailability = asyncHandler(async(req,res) => {
     }
     console.log(bookedSlots)
 
-    const buildTimes = () => {
-        let slots= []
-        
-        for (let i=7; i<22; i++) {
-
-            let booked = false
-            if (bookedSlots.indexOf(i) != -1) {
-                booked = true
-            }
-            
-            let price
-            if (i<18) {
-                price = 50
-            } else {
-                price = 100
-            }
-          slots.push({time: `${i}:00`, value: i, price: price, booked: booked})
-          slots.push({time: `${i}:30`, value: i+0.5, price: price, booked: booked})
-        }
-        return slots
-      }
-      const times = buildTimes()
-      console.log(times)
+    const times = buildTimes(bookedSlots)
+    //console.log(times)
     res.status(200).json({times: times})
 })
 
+//*New booking
 const newBooking = asyncHandler(async(req,res) => {
+
     console.log(req.body)
-    const date= {
-        year: Number(req.body.date.slice(0,4)),
-        month: Number(req.body.date.slice(5,7)),
-        day: Number(req.body.date.slice(8))
+    const date= dateSlicer(req.body.date)
+    if (futureDateChecker(date)) {
+        res.status(400).json({message: "You cannot book in the past. Please select a future date"})
+        return
     }
-    console.log(date)
+
+    //get existing bookings
+    const existingBookings = await Booking.find({year:date.year, month:date.month, day:date.day}).select({slots:1})
+    //console.log(existingBookings)
+    let bookedSlots=[]
+    if (existingBookings) {
+        for (let i=0; i<existingBookings.length; i++) {
+            bookedSlots.push(...existingBookings[i].slots)
+        }
+    }
+    
+    console.log(bookedSlots)
+
+    let total=0
+
+    for (let i=0; i<req.body.slots.length; i++) {
+        console.log(bookedSlots.indexOf(req.body.slots[i].value))
+        if (bookedSlots.indexOf(req.body.slots[i].value) != -1) {
+            res.status(400).json({message: "One of more of the time slots you are trying to book has already been booked. Please retry with a different time."})
+            total = 0
+            console.log('already booked')
+            return
+        } else if (req.body.slots[i].value < 18) {
+            total=total+50
+        } else if (req.body.slots[i].value >= 18) {
+            total=total+100
+        }
+    }
+    
+    console.log(total)
+
+    
+
+
+
+
     const totalAmount = req.body.slots.reduce((total, slot) => {
         return total + slot.price
     },0)
@@ -83,9 +132,9 @@ const newBooking = asyncHandler(async(req,res) => {
         status: 'confirmed'
     })
     console.log(newBooking)
-
-
 })
+
+
 
 module.exports = {
     checkAvailability, newBooking
