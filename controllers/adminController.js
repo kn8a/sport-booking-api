abd31bccfafb5b931eb9f180a80e5b86998f442f
kn8a <mydreamhomefl@gmail.com>
2022-11-09 04:53:29 -0500

    excluded more characters from invite code.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,93 +1,103 @@
 const Booking = require("../models/bookingModel")
 const User = require("../models/userModel")
-const Invite = require('../models/inviteModel')
-const Log = require('../models/logModel')
+const Invite = require("../models/inviteModel")
+const Log = require("../models/logModel")
 const asyncHandler = require("express-async-handler")
-const axios = require("axios");
+const axios = require("axios")
 
-const randomString = require('random-string');
+const randomString = require("random-string")
 
-const generateCode = asyncHandler(async(req,res) => {
-    
-    //validate address format
-    function useRegex(input) {
-        let regex = /\d\d\/\d\d\d/i;
-        return regex.test(input);
-    }
+const generateCode = asyncHandler(async (req, res) => {
+  //validate address format
+  function useRegex(input) {
+    let regex = /\d\d\/\d\d\d/i
+    return regex.test(input)
+  }
 
-    if (!useRegex(req.body.address)) {
-        res.status(400).json({message: 'Incorrect address format'})
-        return
-    }
+  if (!useRegex(req.body.address)) {
+    res.status(400).json({ message: "Incorrect address format" })
+    return
+  }
 
-    //check if address already has code
-    const alreadyExists = await Invite.findOne({address: req.body.address})
+  //check if address already has code
+  const alreadyExists = await Invite.findOne({ address: req.body.address })
 
-    //generate new code
-    const code = randomString({
-        length: 6,
-        numeric: true,
-        letters: true,
-        exclude: ['0','O',"o",'l','I','L',"1"]
+  //generate new code
+  const code = randomString({
+    length: 6,
+    numeric: true,
+    letters: true,
+    exclude: ["0", "O", "o", "l", "I", "L", "1", 'i', 'q'],
+  })
+
+  //if exists update, else create new
+  if (alreadyExists) {
+    await alreadyExists.update({ code: code })
+    res.status(200).json({ code: code })
+    return
+  } else {
+    await Invite.create({
+      address: req.body.address,
+      code: code,
     })
-
-    //if exists update, else create new
-    if (alreadyExists) {
-        await alreadyExists.update({code: code})
-        res.status(200).json({code:code})
-        return
-    } else {
-        await Invite.create({
-            address: req.body.address,
-            code: code
-        })
-        res.status(200).json({code: code})
-    }
+    res.status(200).json({ code: code })
+  }
 })
 
-const confirmAdmin = asyncHandler(async(req,res)=> {
-    res.status(200).json({admin: true})
+const confirmAdmin = asyncHandler(async (req, res) => {
+  res.status(200).json({ admin: true })
 })
 
-const lookupUsersTopUp = asyncHandler(async(req,res)=> {
-    const users = await User.find({status: 'approved', role:'admin'}).select({address:1, name_first:1, name_last:1, email:1, balance:1})
-    res.status(200).json({users:users})
+const lookupUsersTopUp = asyncHandler(async (req, res) => {
+  const users = await User.find({ status: "approved", role: "admin" }).select({
+    address: 1,
+    name_first: 1,
+    name_last: 1,
+    email: 1,
+    balance: 1,
+  })
+  res.status(200).json({ users: users })
 })
 
-const TopUp = asyncHandler(async(req,res)=> {
-    
-    const {userId, amount, receipt} = req.body
-    console.log(userId, amount, receipt)
-    if (!userId) {
-        res.status(400).json({message:'Please select a user and try again'})
-        return
-    }
-    if (!amount || Number(amount) % 50 != 0) {
-        res.status(400).json({message:'Please enter valid amount and try again'})
-        return
-    }
-    if (!receipt) {
-        res.status(400).json({message:'Please enter a receipt number'})
-        return
-    }
+const TopUp = asyncHandler(async (req, res) => {
+  const { userId, amount, receipt } = req.body
+  console.log(userId, amount, receipt)
+  if (!userId) {
+    res.status(400).json({ message: "Please select a user and try again" })
+    return
+  }
+  if (!amount || Number(amount) % 50 != 0) {
+    res.status(400).json({ message: "Please enter valid amount and try again" })
+    return
+  }
+  if (!receipt) {
+    res.status(400).json({ message: "Please enter a receipt number" })
+    return
+  }
 
-    const user = await User.findById(userId)
-    if (!user) {
-        res.status(400).json({message:'This user does not exist'})
-        return
-    }
-    const newBal = user.balance+Number(amount)
-    const updatedUser = await user.update({balance:newBal})
-    const log = await Log.create({
-        user_address: user.address,
-        user_email: user.email,
-        type: "topup",
-        text: `Admin (${req.user.name_first}) added ${amount} to ${user.address} (${user.name_first}, ${user.name_last}). Receipt number is ${receipt}. User's new balance is ${newBal}`
+  const user = await User.findById(userId)
+  if (!user) {
+    res.status(400).json({ message: "This user does not exist" })
+    return
+  }
+  const newBal = user.balance + Number(amount)
+  const updatedUser = await user.update({ balance: newBal })
+  const log = await Log.create({
+    user_address: user.address,
+    user_email: user.email,
+    type: "topup",
+    text: `Admin (${req.user.name_first}) added ${amount} to ${user.address} (${user.name_first}, ${user.name_last}). Receipt number is ${receipt}. User's new balance is ${newBal}`,
+  })
+  res
+    .status(200)
+    .json({
+      message: `Added ${amount} to ${user.address}. New balance: ${newBal}`,
     })
-    res.status(200).json({message: `Added ${amount} to ${user.address}. New balance: ${newBal}`})
-
 })
 
 module.exports = {
-    generateCode, confirmAdmin, lookupUsersTopUp, TopUp
-  }
+  generateCode,
+  confirmAdmin,
+  lookupUsersTopUp,
+  TopUp,
+}
